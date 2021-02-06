ef3c12f586fe2b5a4269f4f20d587107782c91a0
huanghu0 <1525273035@qq.com>
2021-02-06 22:59:38 +0800

    添加vue-router

---
+++ frontend/beyisheji-vue/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './views/home'
+Vue.use(VueRouter);
+const routes = [
+    {
+        path:'/',
+        component:Home
+    }
+];
+export default new VueRouter({
+    mode: 'history',
+    routes,
+});
