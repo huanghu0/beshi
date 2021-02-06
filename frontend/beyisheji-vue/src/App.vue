d589cdebf712daf005cd5fad52f0eb5fb89b396a
huanghu0 <1525273035@qq.com>
2021-02-07 00:22:16 +0800

    引入webpack,ant design,axios

--- frontend/beyisheji-vue/src/App.vue
+++ frontend/beyisheji-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-link to='/'>首页</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -8,6 +8,9 @@
 
 export default {
   name: 'App',
+  mounted(){
+    console.log(this.axios);
+  }
 }
 </script>
 
