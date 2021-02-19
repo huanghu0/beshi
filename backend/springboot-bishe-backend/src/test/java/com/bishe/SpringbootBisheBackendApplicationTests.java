d66ee588ec248f1ade45b46cf81a8adb2dd4bbdd
huanghu0 <1525273035@qq.com>
2021-02-20 00:42:09 +0800

    spring-boot文件创建

--- backend/springboot-bishe-backend/src/test/java/com/bishe/SpringbootBisheBackendApplicationTests.java
+++ backend/springboot-bishe-backend/src/test/java/com/bishe/SpringbootBisheBackendApplicationTests.java
@@ -1,13 +1,20 @@
 package com.bishe;
 
+import com.bishe.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootBisheBackendApplicationTests {
-
+    @Autowired
+    private User user;
     @Test
     void contextLoads() {
+        user.setName("黄虎");
+        user.setAge(22);
+        user.setSex(1);
+        System.out.println(user);
     }
 
 }
