a98f9b62440cbc54a03bf64ed034e7bd825bf138
huanghu0 <1525273035@qq.com>
2021-02-19 11:50:51 +0800

    spring-boot后端搭建 ----未完成所有的整合

---
+++ backend/springboot-bishe-backend/src/main/java/com/bishe/SpringbootBisheBackendApplication.java
@@ -0,0 +1,15 @@
+package com.bishe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SpringbootBisheBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBisheBackendApplication.class, args);
+    }
+}
---
+++ backend/springboot-bishe-backend/src/test/java/com/bishe/SpringbootBisheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBisheBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
