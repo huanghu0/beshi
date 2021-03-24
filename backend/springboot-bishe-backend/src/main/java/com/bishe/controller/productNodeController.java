60cd912c9a410fc07b99b074d442208c0c2d0aea
huanghu0 <1525273035@qq.com>
2021-03-24 23:42:11 +0800

    后端添加节点优化

--- backend/springboot-bishe-backend/src/main/java/com/bishe/controller/productNodeController.java
+++ backend/springboot-bishe-backend/src/main/java/com/bishe/controller/productNodeController.java
@@ -26,19 +26,16 @@ public class productNodeController {
         productNodeMapper.insert(productnode);
     }
     @RequestMapping("/addproductnode")
-    public String addproductnode(String productnodenumber,String productnumber,String parentnodenumber,String productNodeName,int lft,int rht){
-        int abs = rht - lft;
+    public String addproductnode(String productnodenumber,String productnumber,String parentnodenumber,String productNodeName){
         List<Productnode> product =  productNodeMapper.selectByNumber(productnumber);
         List<Productnode> parentnode =  productNodeMapper.selectByNumber(parentnodenumber);
-        if(abs != 1){
-            return "添加失败,节点rht必须只比lft值大1";
-        }else if(product == null || product.isEmpty()){
-            return "添加失败,节点所要添加到的对应产品模板不存在，请检查对应产品是否存在!";
+        if(product == null || product.isEmpty()){
+            return "添加失败,节点所要添加到的对应产品模板不存在，请检查对应产品编号是否正确!";
         }else if(parentnode == null || parentnode.isEmpty()){
-            return "添加失败,父节点不存在,请检查父节点是否天写正确!";
-        }else if(parentnode.get(0).getLft()>= lft || parentnode.get(0).getRht() < lft){
-            return "添加失败,节点的lft必须大于父节点的lft小于等于父节点的rht,当前父节点的lft和rht分别为"+parentnode.get(0).getLft()+','+parentnode.get(0).getRht();
+            return "添加失败,父节点不存在,请检查父节点号是否正确!";
         }
+        int lft = parentnode.get(0).getRht();
+        int rht = lft+1;
         productNodeMapper.updatenodelft(lft);
         productNodeMapper.updatenoderht(lft);
         addProductNode(productnodenumber,productnumber,parentnodenumber,productNodeName,lft,rht);
